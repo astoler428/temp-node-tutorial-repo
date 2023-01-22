0dbe6b24725e8fc476cc51556472821be923d767
Ari Stoler <astoler428@gmail.com>
2023-01-22 17:44:56 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") res.write("This is my homepage");
+//   if (req.url === "/about") res.write("This is the about page");
+//   res.end(`<h1>No page found<h1>`);
+//   console.log("past end");
+//   // res.end(`<h1>This page doesn't exist</h1>`);
+// });
+
+// server.listen(5000);
